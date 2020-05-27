8d678db2b4774f14405339365a02f2f4083bfad0
Pratya chugh <41120205+pratyaa@users.noreply.github.com>
2020-05-27 20:04:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import Form from './form.js';
-
+import Form from './components/form2.js';
+import Appbar from "./components/Navbar.js";
+import Footer from "./components/Footer.js";
 
 function App() {
   return (
+    <React.Fragment>
+    <Appbar/>
     <Form />
+    <Footer/>
+    </React.Fragment>
   );
 }
 
 export default App;
+
