f2530b1c0201f07e13308b2ee09b921c850e46cb
Yash Tayal <yashtayal341@gmail.com>
2020-06-15 13:23:02 +0530

    Updates

---
+++ precache-manifest.83fa52f7d342d26250b2b221e827c97f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd9b30f332f2c784ba85bc7486cb9561",
+    "url": "/automate_Report/index.html"
+  },
+  {
+    "revision": "1d2796f903b02fe50f7e",
+    "url": "/automate_Report/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "006207a7601fcc44436b",
+    "url": "/automate_Report/static/css/main.b295243d.chunk.css"
+  },
+  {
+    "revision": "1d2796f903b02fe50f7e",
+    "url": "/automate_Report/static/js/2.cea8e20d.chunk.js"
+  },
+  {
+    "revision": "f06eb3a8f47b20bd321ac0a3e4f2028a",
+    "url": "/automate_Report/static/js/2.cea8e20d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "006207a7601fcc44436b",
+    "url": "/automate_Report/static/js/main.9faaf290.chunk.js"
+  },
+  {
+    "revision": "a5be4552cdb400a27907",
+    "url": "/automate_Report/static/js/runtime-main.f32ae0ae.js"
+  },
+  {
+    "revision": "e5325eb2ca49e572b35ec8f4a59c2b04",
+    "url": "/automate_Report/static/media/logo.e5325eb2.svg"
+  }
+]);
